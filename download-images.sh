3a7234d963a4f8e59850cf194816e36baff0ec11
Peter Danenberg <pcd@roxygen.org>
2013-09-09 14:20:28 -0700

    Optional echo

--- download-images.sh
+++ download-images.sh
@@ -3,6 +3,7 @@
 # [[file:~/prg/org/hadoop-images/TODO.org::*Streaming][Streaming:1]]
 
 while read url; do
+    # echo $url >&2
     size=$(curl -s -D - -o /dev/null "$url" | \
         grep -i content-length | \
         cut -d ' ' -f 2)
