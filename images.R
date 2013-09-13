94916da6621e83959f90d1515f27788d61469cbc
Peter Danenberg <pcd@roxygen.org>
2013-09-13 11:01:14 -0700

    Plot the data.

---
+++ images.R
@@ -0,0 +1,10 @@
+library(debug)
+library(ggplot2)
+data <- rbind(read.csv('images-hadoop.csv', header=F, col.names=c('time', 'index', 'bytes'), sep=' '),
+              read.csv('images-local.csv', header=F, col.names=c('time', 'index', 'bytes'), sep=' '))
+png("images.png")
+ggplot(data=data, aes(x=time, y=bytes, col=index)) +
+  geom_point() +
+  geom_smooth()
+
+dev.off()
